76d77e7e5f61c4f2e540cb575225195077d9c05b
guidomperosino <guidomperosino@gmail.com>
2020-11-19 12:54:32 -0300

    "incorporacion de Internationalizacion"

---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="masthead">
+    <div class="container">
+      <div class="masthead-subheading">{{ $t("welcomeMsg") }}</div>
+      <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
+      <a
+        class="btn btn-primary btn-xl text-uppercase js-scroll-trigger"
+        href="#services"
+        >Tell Me More</a
+      >
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    navbarJSON: [{}],
+  }),
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <!-- Navigation-->
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top"
+        ><img :src='navbarCustom.imgSrc' :alt='navbarCustom.imgAlt'
+      />{{ navbarCustom.title }}</a>
+      <button
+        class="navbar-toggler navbar-toggler-right"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarResponsive"
+        aria-controls="navbarResponsive"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        Menu
+        <i class="fas fa-bars ml-1"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav text-uppercase ml-auto">
+          <li class="nav-item" v-for="(section, key) in navbarJSON" :key="key">
+            <a class="nav-link js-scroll-trigger" :href="section.link">{{ section.title }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    navbarJSON: [
+      {
+        title: "section1",
+        link: "#section1",
+      },
+      {
+        title: "section2",
+        link: "#section2",
+      },
+      {
+        title: "section3",
+        link: "#section3",
+      },
+      {
+        title: "section4",
+        link: "#section4",
+      },
+    ],
+    navbarCustom: {
+      imgSrc: 'assets/img/logo.png',
+      imgAlt: 'logo',
+      title:"GT Comex"
+    },
+  }),
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
